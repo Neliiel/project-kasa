5532ffbc8d2eaee834bb562df696b2c40576b981
Nellie <n.miannay@orange.fr>
2023-03-10 19:21:24 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from "./pages/Home";
+import About from './pages/About';
+import Error from './pages/Error';
+import Fiche from './pages/Fiche';
+
+import Header from './components/Header/index';
+import Footer from './components/Footer/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <Router>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/About">
+            <About />
+          </Route>
+          <Route path="/Fiche">
+            <Fiche />
+          </Route>
+          <Route path="/Error">
+            <Error />
+          </Route>
+        </Router>
+      </main>
+      < Footer />
     </div>
   );
 }
