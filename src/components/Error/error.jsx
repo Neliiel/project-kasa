aa28366e98094a966c7167d45ce120ef368e1375
Nellie <n.miannay@orange.fr>
2023-03-18 19:50:40 +0100

    Mise en place page Error

--- src/components/Error/error.jsx
+++ src/components/Error/error.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function Errorpage () {
     return (
         <div className="error-page">
             <p className="error-number">404</p>
             <p className="oups"> Oups! La page que vous demandez n'existe pas</p>
-            <p className="return-home">Retourner sur la page d'accueil</p>
+            <Link to="/" className="nav-link">Retourner sur la page d'accueil</Link>
         </div>
     )
 }
